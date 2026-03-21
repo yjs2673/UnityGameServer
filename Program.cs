72511d2e4ced3afef9325e069cc8cf27f0136b6d
Yun Junseo <yjs2673@naver.com>
2026-03-21 20:49:57 +0900

    라이브러리 추가 및 설정

--- Program.cs
+++ Program.cs
@@ -3,32 +3,48 @@ using MyGameServer.Models;
 using StackExchange.Redis;
 using Microsoft.AspNetCore.Mvc;
 using MyGameServer;
+using MyGameServer.Services;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// DB 연결
+// DB 연결 설정
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// MySQL 등록 (Pomelo)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 
-// Redis 연결 설정 추가
-var redis = ConnectionMultiplexer.Connect("localhost");
-builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
+// Redis 분산 캐시(IDistributedCache) 등록
+builder.Services.AddStackExchangeRedisCache(options =>
 {
-    options.SuppressModelStateInvalidFilter = true; // 자동 에러 응답 비활성화
+    options.Configuration = "localhost:6379";
+    options.InstanceName = "MyGame_";
 });
 
-var chatServer = new ChatServer();
-_ = Task.Run(() => chatServer.Start()); // 별도 테스크로 소켓 서버 실행
+// Redis 연결 객체(IConnectionMultiplexer) 등록
+var redis = ConnectionMultiplexer.Connect("localhost:6379");
+builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
+
+// API 유효성 검사 커스텀 설정
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+// ChatServer 싱글톤 등록
+builder.Services.AddSingleton<ChatServer>();
+
+// 서버 시작 시 Redis 세션을 정리하는 Hosted Service 등록
+builder.Services.AddHostedService<RedisInitService>();
 
 var app = builder.Build();
-// app.UseHttpsRedirection();
+
+// 채팅 서버 인스턴스 가져오기 및 시작
+var chatServer = app.Services.GetRequiredService<ChatServer>();
+_ = chatServer.Start(); 
+
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
