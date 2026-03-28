428f7c0303973808002df15a8e353b73866151cc
Yun Junseo <yjs2673@naver.com>
2026-03-28 23:20:55 +0900

    DB 직접 연결 로직 추가

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,12 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using MyGameServer.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    // API 서버 생성자를 사용하도록 강제 지
+    [ActivatorUtilitiesConstructor]
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
-    // DB 테이블 매핑
-    public DbSet<User> Users { get; set; }          // 유저 테이블
-    public DbSet<Item> Items { get; set; }          // 아이템 테이블
-    public DbSet<UserItem> UserItems { get; set; }  // 유저 아이템 테이블
+    // 소켓 서버 생성자
+    public AppDbContext() {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<UserItem> UserItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // DB 연결
+            string connStr = "Server=localhost;Database=GameDB;Uid=root;Pwd=2673;";
+            var serverVersion = new MySqlServerVersion(new System.Version(8, 0, 45));
+            optionsBuilder.UseMySql(connStr, serverVersion);
+        }
+    }
 }
